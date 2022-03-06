9f2c73fc57d56a300213e2320d1e77f585cdd18b
davidzhongsydney <david.zhong.uts@gmail.com>
2022-03-06 19:59:55 +1100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './pages/Home'
+import CredentialView from './pages/CredentialView';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Link to="/">Home</Link> <br/>
+        <Link to="/credential/Tom">Credential Tom</Link><br/>
+        <Link to="/credential/Jerry">Credential Jerry</Link>
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home  />
+        </Route>
+
+        <Route path="/credential/:name">
+          <CredentialView  />
+        </Route>
+      </Switch>
+    </Router>
+);
+}
+
+export default App;
